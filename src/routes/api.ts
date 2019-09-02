6d30426e4c62d9fbc8e3ad4d17480ed68a092d5a
Daniel Gulic <danielgulic@gmail.com>
2019-09-02 21:25:07 +1000

    errors are now json + 2hr max length

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,15 +5,18 @@ import ffmpeg from 'fluent-ffmpeg';
 const router = Router();
 
 const formats = ['mp3', 'mp4'];
+const maxLength = 7200; // 2 hours in seconds
 const ytRegex = /^(http(s)?:\/\/)((w){3}.)?youtu(be|.be)?(\.com)?\/watch\?v=[a-zA-Z0-9]+/;
 
 router.get('/convert', async (req: Request, res: Response) => {
 	const { url, format } = req.query as { url: string; format: string };
-	if (!format || !url) return res.sendStatus(400);
-	if (!formats.includes(format.toLowerCase())) return res.sendStatus(400);
-	if (!ytRegex.test(url)) return res.sendStatus(400);
+	if (!format || !url) return res.status(400).json({ error: 'Missing parameters' });
+	if (!formats.includes(format.toLowerCase())) return res.status(400).json({ error: `The format provided is not a valid format; accepted formats are: ${formats.join(', ')}` })
+	if (!ytRegex.test(url)) return res.status(400).json({ error: 'The url provided is not a YouTube video' })
 
 	const info = await ytdl.getBasicInfo(url);
+	const length = parseInt(info.length_seconds);
+	if (length > maxLength) return res.status(400).json({ error: 'The video must be under 2 hours long' });
 	console.log(info);
 	const slug = info.title.split(' ').join('_') + '_danielwashere';
 	res.setHeader(
@@ -29,6 +32,7 @@ router.get('/convert', async (req: Request, res: Response) => {
 				.format('mp3')
 				.audioBitrate(128)
 				.pipe(res);
+			break;
 	}
 });
 
