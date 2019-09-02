4255b7cc71870cc82279f53631d0c53283bced51
Daniel Gulic <danielgulic@gmail.com>
2019-09-02 21:30:15 +1000

    make format lowercase

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -9,11 +9,12 @@ const maxLength = 7200; // 2 hours in seconds
 const ytRegex = /^(http(s)?:\/\/)((w){3}.)?youtu(be|.be)?(\.com)?\/watch\?v=[a-zA-Z0-9]+/;
 
 router.get('/convert', async (req: Request, res: Response) => {
-	const { url, format } = req.query as { url: string; format: string };
+	let { url, format } = req.query as { url: string; format: string };
 	if (!format || !url) return res.status(400).json({ error: 'Missing parameters' });
 	if (!formats.includes(format.toLowerCase())) return res.status(400).json({ error: `The format provided is not a valid format; accepted formats are: ${formats.join(', ')}` })
 	if (!ytRegex.test(url)) return res.status(400).json({ error: 'The url provided is not a YouTube video' })
 
+	format = format.toLowerCase();
 	const info = await ytdl.getBasicInfo(url);
 	const length = parseInt(info.length_seconds);
 	if (length > maxLength) return res.status(400).json({ error: 'The video must be under 2 hours long' });
