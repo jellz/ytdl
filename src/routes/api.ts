c760bdf3cdd0efbe1c24a9066858746d3684fbbf
Daniel Gulic <danielgulic@gmail.com>
2019-09-02 23:06:36 +1000

    block livestreams + validate urls more

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -13,17 +13,24 @@ router.get('/convert', async (req: Request, res: Response) => {
 	if (!format || !url) return res.status(400).json({ error: 'Missing parameters' });
 	if (!formats.includes(format.toLowerCase())) return res.status(400).json({ error: `The format provided is not a valid format; accepted formats are: ${formats.join(', ')}` })
 	if (!ytRegex.test(url)) return res.status(400).json({ error: 'The url provided is not a YouTube video' })
+	if (!ytdl.validateURL(url)) return res.status(400).json({ error: 'Invalid video ID' });
 
 	format = format.toLowerCase();
+
 	const info = await ytdl.getBasicInfo(url);
 	console.log(info.title);
+	const isLive = info.player_response.videoDetails.isLiveContent;
+	if (isLive) return res.status(400).json({ error: 'Livestreams are currently not supported' });
+
 	const length = parseInt(info.length_seconds);
 	if (length > maxLength) return res.status(400).json({ error: 'The video must be under 2 hours long' });
+
 	const slug = info.title.split(' ').join('_') + '_danielwashere';
 	res.setHeader(
 		'Content-Disposition',
 		`attachment; filename=${slug}.${format}`
 	);
+	
 	switch (format) {
 		case 'mp4':
 			ytdl(url).pipe(res);
